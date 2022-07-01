61cba753887487bbff2b6e8d57147d886454ac9c
MinaAhmadi77 <mina412@aut.ac.ir>
2022-07-02 01:49:09 +0430

    changed

--- server.py
+++ server.py
@@ -17,13 +17,13 @@ try:
             print(data)
 except:
 
-    data['expiretime']= 5
+    data['minutes_to_expire']= 5
     data['port'] = 8000
-    data['DBusername'] = "mina412"
-    data['DBpassword'] = "minaahmadi77"
-    data['DBurl'] = "@project.itbja.mongodb.net/?retryWrites=true&w=majority"
+    data['user'] = "mina412"
+    data['pass'] = "minaahmadi77"
+    data['url'] = "@project.itbja.mongodb.net/?retryWrites=true&w=majority"
 
-client = pymongo.MongoClient("mongodb+srv://"+data['DBusername']+":"+data['DBpassword']+data['DBurl'])
+client = pymongo.MongoClient("mongodb+srv://"+data['user']+":"+data['pass']+data['url'])
 
 db = client['database']
 
@@ -47,16 +47,17 @@ def warningPage(id):
     note = notes.find_one({"id":id})
     if(note['available']) == True:
         spend = (datetime.now() - note['time']).total_seconds()
-        if (spend > data['expiretime'] * 60):
+        if (spend > data['minutes_to_expire'] * 60):
             notes.update_one({"id":id},{ "$set": { 'available': False } })
-            return "404 not found"
+            return "not available"
         return render_template("warning_page.html", id = id)
-
+    else:
+        return "not available"
 @app.route("/showNotePage",methods=["POST"])
 def showNotePage():
-    id = request.form['btn'] # TODO ino bayad doros konim------------------------------
+    id = request.form['btn']
     notes.update_one({"id":id},{ "$set": { 'available': False } })
-    print("ineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", id)
+    print("id", id)
     return render_template("show_note_page.html", text = notes.find_one({"id":id})['note'])
 
 if __name__ == "__main__":
